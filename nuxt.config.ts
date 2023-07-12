b6f4eb421da079ed66ada56a7d7ac852bad70827
Mariangel Moya <moya.mariangel.jose@gmail.com>
2023-07-12 17:11:50 +0200

    Some changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    // @ts-ignore
+    css: ['~/assets/css/main.css'],
+    runtimeConfig: {
+        public: {
+            api: process.env.DOMAIN
+        }
+    },
     devtools: {enabled: true},
     modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
-    css: ['~/assets/css/main.css'],
     colorMode: {
         preference: 'system', // default theme
         dataValue: 'theme', // activate data-theme in <html> tag
